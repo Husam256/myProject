a7a47c0b95eb53c76646adb9a155319c93cfa36d
Husam256 <62894890+Husam256@users.noreply.github.com>
2021-02-03 13:08:28 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class HelloWorldApp extends Component{
+  render(){
+
+let name = "Husam";
+
+    return(
+      <View>
+        <Text>Hello hello</Text>
+      </View>
+    );
+
+  }
+}
+export default HelloWorldApp;
